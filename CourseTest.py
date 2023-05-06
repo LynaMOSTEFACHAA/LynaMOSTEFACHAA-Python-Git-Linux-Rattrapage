4415522de4f1f0f6fb235118be5414eb5362de38
Lyna <lyna.mostefa_chaa@edu.devinci.fr>
2023-05-06 19:50:13 +0200

    fix: bash in callback; random question button

--- CourseTest.py
+++ CourseTest.py
@@ -12,13 +12,13 @@ answers = {
 
 def get_random_question():
     # Appeler le script bash et récupérer la sortie
-    output = subprocess.check_output(['bash', 'RandomQuestion.sh'])
+    output = subprocess.check_output(['bash', '/home/naseem/Bureau/Projet2/RandomQuestion.sh'])
 
     # Convertir la sortie en chaîne de caractères et la diviser en question et concept
-    print(output.decode("utf-8").strip().split(";"))
     question, concept = output.decode("utf-8").strip().split(",")
     return question, concept
 
+#Initialisation
 question, concept = get_random_question()
 
 app = dash.Dash(__name__)
@@ -29,6 +29,7 @@ app.layout = html.Div([
     html.P(id="question", children=question),
     dcc.Input(id="answer-input", type="text", placeholder="Entrez votre réponse"),
     html.Button("Evaluate answer", id="submit-answer"),
+    html.Button("Random question", id="random-question"),
     html.Div(id="result")
 ])
 
@@ -37,15 +38,18 @@ app.layout = html.Div([
      dash.dependencies.Output("answer-input", "value"),
      dash.dependencies.Output("question", "children"),
      dash.dependencies.Output("concept", "children")],
-    [dash.dependencies.Input("submit-answer", "n_clicks")],
+    [dash.dependencies.Input("submit-answer", "n_clicks"),
+     dash.dependencies.Input("random-question", "n_clicks")],
     [dash.dependencies.State("answer-input", "value"),
      dash.dependencies.State("concept", "children")])
-def update_question(n_clicks, value, concept):
+def update_question(submit_clicks, random_clicks, value, concept):
     global question
     ctx = dash.callback_context
     if not ctx.triggered:
         # Premier chargement de la page
-        question, concept = get_random_question()
+        process = subprocess.Popen(['bash', '/home/naseem/Bureau/Projet2/RandomQuestion.sh'], stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        question, concept = output.decode("utf-8").strip().split(",")
         return "", "", question, concept
     else:
         button_id = ctx.triggered[0]["prop_id"].split(".")[0]
@@ -54,15 +58,16 @@ def update_question(n_clicks, value, concept):
             if not value:
                 return "", "", question, concept
             elif value.lower() in answers[concept].lower():
-                question, concept = get_random_question()
+                process = subprocess.Popen(['bash', '/home/naseem/Bureau/Projet2/RandomQuestion.sh'], stdout=subprocess.PIPE)
+                output = process.communicate()[0]
+                question, concept = output.decode("utf-8").strip().split(",")
                 return "Réponse correcte!", "", question, concept
             else:
                 return "Réponse incorrecte. Veuillez réessayer.", value, question, concept
-        else:
-            # Obtenir une nouvelle question
+        elif button_id == "random-question":
+            # Obtenir une nouvelle question aléatoire
             question, concept = get_random_question()
             return "", "", question, concept
-
 
 
 if __name__ == '__main__':
